a81c47148a69e6649eeefccd80318c7adf8b9df1
Shaurya Bansal <shauryabansal537@gmail.com>
2024-11-11 20:25:23 +0530

    .gitignored test.py

--- Python Programs/test.py
+++
@@ -1,8 +0,0 @@
-import pickle
-file = open('records.dat', 'wb')
-record=[{'id': 1, 'name': 'Shaurya', 'age': 17, 'department': 'IT'}]
-pickle.dump(record, file)
-
-file = open('records.dat', 'rb')
-records=pickle.load(file)
-print(records)
